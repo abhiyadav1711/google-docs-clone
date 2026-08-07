d6b54dae5de24b96b2d386ea26eca085cfb03af2
abhiyadav1711 <abhinavyadav1711@gmail.com>
2026-08-07 10:47:47 +0530

    Update urls.py

--- editor/urls.py
+++ editor/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.document_list, name='document_list'),
     path('new/', views.document_create, name='document_create'),
     path('document/<int:pk>/', views.document_edit, name='document_edit'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
