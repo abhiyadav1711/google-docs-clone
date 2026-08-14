4b57b139d727a89ef250b05ffc97df4df2fb17c3
abhiyadav1711 <abhinavyadav1711@gmail.com>
2026-08-14 16:42:52 +0530

    Update settings.py

--- docsclone/settings.py
+++ docsclone/settings.py
@@ -58,3 +58,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'editor/static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
+CSRF_TRUSTED_ORIGINS = ['https://*.app.github.dev']
